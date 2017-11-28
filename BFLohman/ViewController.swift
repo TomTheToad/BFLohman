978622b985bf3de34be6454b532be95adde1d024
TomTheToad <tomthetoad951@gmail.com>
2017-11-28 08:43:51 -0500

    Initial commit for bodyFat calculator for wife's work. App is working as expected.

--- BFLohman/ViewController.swift
+++ BFLohman/ViewController.swift
@@ -8,18 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // IBOutlets
+    @IBOutlet weak var bodyWeight: UITextField!
+    @IBOutlet weak var abdomen: UITextField!
+    @IBOutlet weak var tricep: UITextField!
+    @IBOutlet weak var subscapularis: UITextField!
+    
+    @IBOutlet weak var bodyFat: UILabel!
+    @IBOutlet weak var minWeight: UILabel!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        setBodyFat()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func calculateBodyDensity(sumOfMeasurements: Float) -> Float {
+        return (1.0973 - (sumOfMeasurements * 0.000815)) + (sumOfMeasurements * 0.00000084)
     }
 
-
+    func calculateBodyFatPercentage(bodyDensity: Float) -> Float {
+        return (457 / bodyDensity) - 414.2
+    }
+    
+    func setBodyFat() {
+        
+        guard let thisAB = abdomen.text else {
+            return
+        }
+        
+        guard let thisTri = tricep.text else {
+            return
+        }
+        
+        guard let thisSubscap = subscapularis.text else {
+            return
+        }
+        
+        guard let ab = Float(thisAB) else {
+            return
+        }
+        
+        guard let tri = Float(thisTri) else {
+            return
+        }
+        
+        guard let subscap = Float(thisSubscap) else {
+            return
+        }
+        
+        let bodyDensity = calculateBodyDensity(sumOfMeasurements: ab + tri + subscap)
+        let bodyFatPercentage = calculateBodyFatPercentage(bodyDensity: bodyDensity)
+        
+        bodyFat.text = "\(bodyFatPercentage)"
+        
+        setMinWeight(bodyFatPercentage: bodyFatPercentage)
+    }
+    
+    func setMinWeight(bodyFatPercentage: Float) {
+        guard let bodyWeight = Float(bodyWeight.text!) else {
+            return
+        }
+        
+        let min = ((1 - (bodyFatPercentage / 100)) * bodyWeight) / (0.93)
+        
+        minWeight.text =  "\(min)"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
